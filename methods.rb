9ee1787c339f621f015ed9068f18bb7e72335e1e
Chloe <chloepeng331@icloud.com>
2022-03-13 09:15:01 -0400

    commit tree 5a117accd2b9823c768a00c8077be3677f861c76

--- methods.rb
+++ methods.rb
@@ -1,1 +1,53 @@
-# Your code here!
+# function greetProgrammer() {
+#   console.log("Hello, programmer!");
+# }
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+# function greet(name) {
+#   console.log(`Hello, ${name}!`);
+# }
+def greet (name)
+    puts "Hello, #{name}!"
+end
+
+# function greetWithDefault(name = "programmer") {
+#   console.log(`Hello, ${name}!`);
+# }
+def greet_with_default (name ="programmer")
+    puts "Hello, #{name}!"
+end
+
+# function add(num1, num2) {
+#   return num1 + num2;
+# }
+def add(num1, num2)
+    return num1 + num2;
+end
+
+# function halve(number) {
+#   if (typeof number !== "number") return null;
+
+#   return number / 2;
+# }
+def halve number
+    if number.class != Integer
+        return nil
+    end
+    
+    return number.div(2)
+end
+
+# def reverse_name(name)
+#     if name.class != String
+#       return nil
+#     end
+  
+#     name.reverse
+#   end
+
+# value after performing division 
+# puts num1.div(num2)
+# puts num3.div(num4)
+
